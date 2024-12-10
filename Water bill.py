dffe11389e6c5acf89535eedcb33422b9b47324b
Catsup-Mina <paolo.vince45@gmail.com>
2024-12-11 00:08:34 +0800

    Update Water bill.py

--- Water bill.py
+++ Water bill.py
@@ -1,39 +1,42 @@
-from decimal import Decimal
-
+from decimal import *
+getcontext().prec = 5
 
 Previous = 356
 Present = 388
-Half = 218.40
-TQ = 349.40     #Three Quarters
-Whole = 698.85
-WHalf = 1747.20
-Two = 4368.00
-Three = 7862.40
-Four = 15724.80
-Ten = 60278.40
+Half = Decimal('218.40')
+TQ = Decimal('349.40')     #Three Quarters
+Whole = Decimal('698.85')
+WHalf = Decimal('1747.20')
+Two = Decimal('4368.00')
+Three = Decimal('7862.40')
+Four = Decimal('15724.80')
+Ten = Decimal('60278.40')
 CuM = Present - Previous
-Amount = Half
 
 def bracketing():
     global CuM
-    global Amount
+    global Half
+    Amount = Half
     while CuM > 10:
         if CuM > 41:
-            while CuM > 41:
+            while CuM >= 41:
                 CuM -= 1
-                Amount += 36.00
+                Amount = Decimal(Amount) + Decimal('36.00')
         elif CuM > 31:
-            while CuM > 31:
+            while CuM >= 31:
                 CuM -= 1
-                Amount += 33.65
+                Amount = Decimal(Amount) + Decimal('33.65')
+                print(Amount)
         elif CuM > 21:
-            while CuM > 21:
+            while CuM >= 21:
                 CuM -= 1
-                Amount += 31.85
+                Amount = Decimal(Amount) + Decimal('31.85')
+                print(Amount)
         elif CuM > 11:
             while CuM >= 11:
                 CuM -= 1
-                Amount += 30.55
+                Amount = Decimal(Amount) + Decimal('30.55')
+                print(Amount)
             print(CuM)
-            print(Decimal(str(Amount)))            
+            print(Amount)            
 bracketing()
